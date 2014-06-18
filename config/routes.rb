f1a8a27fdc96df82d9aa6c48f433714c7e626cea
John Keith <johnkeith@outlook.com>
2014-06-18 16:25:56 -0400

    Omni auth added. Need to test
    Tutorial on testing omniauth sessions
    http://natashatherobot.com/rails-test-omniauth-sessions-controller/

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get '/auth/github/callback', to: 'sessions#create'
+
+  resources :sessions, only: [:destroy]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
